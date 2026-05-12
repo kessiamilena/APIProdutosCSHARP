e7395fee44dd8631fb40b8ece887cd612d130b46
kessiamilena <kessia.milena124@yahoo.com>
2026-05-12 14:06:42 -0300

    :tada: init: Commit inicial

---
+++ APIProdutos/Contexts/ProdutoContext.cs
@@ -0,0 +1,14 @@
+﻿using APIProdutos.Domains;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIProdutos.Contexts
+{
+    public class ProdutoContext : DbContext
+    {
+        public ProdutoContext(DbContextOptions<ProdutoContext> options) : base(options) { }
+
+        // DbSet -> "Crie uma tabela produtos na classe produto"
+        public DbSet<Produto> Produtos { get; set; }
+
+    }
+}
---
+++ APIProdutos/Domains/Produto.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APIProdutos.Domains
+{
+    public class Produto
+    {
+        // anotação -> Key
+        // serve para dizer que esse atributo é uma chave primária no banco de dados
+        [Key]
+        public int Id { get; set; }
+
+        // anotação -> Required
+        // todos os campos precisam ser obrigatórios
+        [Required]
+        public string Nome { get; set; } = string.Empty;
+
+        [Required]
+        public string Marca { get; set; } = string.Empty;
+
+        // decimal(10,2) -> podemos ter até 10 números armazenados, sendo eles 2 números depois da vírgula
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Preco { get; set; }
+
+        public int QuantidadeEstoque { get; set; }
+
+        public bool Ativo { get; set; } = true;
+
+    }
+}
